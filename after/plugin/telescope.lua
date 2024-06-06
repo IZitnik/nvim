111ae0024b061488095b520fb1c41c1270ca85d7
tekkitakcz <tekkitakcz@gmail.com>
2024-06-06 12:19:19 +0200

    Added venv and pycache to ignored by telescope search

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,12 @@
+local ts = require('telescope').setup{
+	defaults = { 
+		file_ignore_patterns = {
+			"node_modules",
+			"venv",
+			"__pycache__",
+		} 
+	} 
+}
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>pF', builtin.live_grep, {})
