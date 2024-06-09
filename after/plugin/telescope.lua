bede56d05b15733d8313f38814b18e0fedaa21e5
tekkitakcz <tekkitakcz@gmail.com>
2024-06-09 11:33:20 +0200

    Added png to ignored files

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,13 @@
+require('telescope').setup{
+	defaults = {
+		file_ignore_patterns = {
+			"node_modules",
+			"venv",
+			"__pycache__",
+			".png$",
+		}
+	}
+}
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>pF', builtin.live_grep, {})
